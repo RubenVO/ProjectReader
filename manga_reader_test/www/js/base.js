6d8067b7c9a66576e8e4e8dd4695f9794a07121d
RubenVO <rubenvanostaeyen@hotmail.com>
2016-05-13 00:08:59 +0200

    Update 1
    added basic read chapter function (hard coded naruto chapter 1 Kappa)

--- manga_reader_test/www/js/base.js
+++ manga_reader_test/www/js/base.js
@@ -1,16 +1,59 @@
 (function ( $ ) {
-    $("#getpage").click(function(){
-      console.log("you clicked the getpage button!");
+
+    // TODO: -Browse manga
+    //       -Search manga
+    //       -Follow manga
+    //       -Manga page
+    //          -make "getManga" function that returns manga info and chapter count
+    //       -Actual layout Kappa
+
+    //global variables
+    var chapterObject;
+    var source = "mangafox.me";
+    var manga = "naruto";
+    var chapter = 1;
+    var currentPage;
+
+    /*gets chapter info + displays first page
+    *
+    *object syntax:
+    *   {href:"mangaLink", name: "mangaName", pages: Array[x], lastUpdate: "date"}
+    *
+    *objects in array syntax:
+    *   x: {pageId: x, url: "imgLink"}
+    *   where x is array index
+    *
+    *   TODO: -make temp buttons dissapear when regarding
+    *         -create back button
+    */
+    $("#getChapter").click(function(){
+      console.log("you clicked the getChapter button!");
       $.ajax({
         beforeSend: function(request){
           request.setRequestHeader("X-Mashape-Key", "9bF2fE4br2mshOohWmEcxGeC38Jgp1toGPLjsnWzUL3hxxWvxm");
         },
-        url: "https://doodle-manga-scraper.p.mashape.com/mangafox.me/manga/naruto/1",
+        url: "https://doodle-manga-scraper.p.mashape.com/" + source + "/manga/" + manga + "/" + chapter,
         dataType:"json",
         success: function(json){
-          console.log(json);
-          $("#imgdiv").prepend("<img src='" + json.pages[0].url + "' />")
+          chapterObject = json;
+          console.log(chapterObject);
+          $("#imgdiv").prepend("<img src='" + chapterObject.pages[0].url + "' />")
+          currentPage = 1;
         }
       });
     });
+
+    /*gets next page of current manga chapter
+    *
+    *   TODO: -check for chapter length
+    *         -invoke getChapter function for next chapter on chapter end
+    *         -
+    */
+    $("#imgdiv").on("click", function(){
+      console.log("you clicked the nextPage button!");
+      $("#imgdiv img").attr("src", chapterObject.pages[currentPage + 1].url);
+      currentPage += 1;
+    });
+
+
 }(jQuery));
