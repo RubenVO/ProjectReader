972020e2df73940ee10d7cb7df7a85fed231e935
RubenVO <rubenvanostaeyen@hotmail.com>
2016-05-15 00:38:52 +0200

    Update
    - layout
    - chapter overview basics

--- manga_reader_test/www/js/chapters.js
+++ manga_reader_test/www/js/chapters.js
@@ -2,29 +2,32 @@
 
   var mangaListObject;
   var source = "mangafox.me";
+  var manga = localStorage.mangaId;
   var page = 0;
   var searchVal;
 
   $(document).ready(function(){
     console.log("document ready!");
+
     $.ajax({
       beforeSend: function(request){
         request.setRequestHeader("X-Mashape-Key", "9bF2fE4br2mshOohWmEcxGeC38Jgp1toGPLjsnWzUL3hxxWvxm");
       },
-      url: "https://doodle-manga-scraper.p.mashape.com/" + source + "/search?cover=1",
+      url: "https://doodle-manga-scraper.p.mashape.com/" + source + "/manga/" + manga,
       dataType:"json",
       success: function(json){
         mangaListObject = json;
         console.log(mangaListObject);
-        $.each(mangaListObject, function(index, element){
 
-          //console.log(index);
+        $("#mangaThumbnails").append("<div id='imgdiv'> <img src='" + mangaListObject.cover + "'/></div>");
+        $("#info").append("<p>" + mangaListObject.info + "</p>");
 
-          if(index > ((-1) + (page * 10)) && index < (10 + (page * 10))){
-            $("#mangaThumbnails").append("<div id='imgdiv'> <img id='" + element.mangaId + "' src='" + element.cover + "'/></div>");
-          }
+        $.each(mangaListObject.chapters, function(index, element){
+
+            $("#chapters").append("<div id='chapter'><div id='chapterIndex'>" + element.chapterId + "</div><div id='chapterName'>" + element.name + "</div></div>");
 
         });
+
       }
     });
   });
@@ -35,42 +38,8 @@
     console.log("The manga ID is: " + e.target.id);
     if(e.target.id != "mangaThumbnails" && e.target.id != "imgdiv"){
       localStorage.mangaId = e.target.id;
-      window.location.href = "chapters.html"
+      window.location.href = "reader.html"
     }
-  });
-
-
-
-  $("#search").on("click", function(){
-    searchVal = $("#searchBox").val();
-    console.log($("#searchBox").val());
-    $("#mangaThumbnails").empty();
-
-    $.ajax({
-      beforeSend: function(request){
-        request.setRequestHeader("X-Mashape-Key", "9bF2fE4br2mshOohWmEcxGeC38Jgp1toGPLjsnWzUL3hxxWvxm");
-      },
-      url: "https://doodle-manga-scraper.p.mashape.com/" + source + "/search?cover=1&q=" + searchVal,
-      dataType:"json",
-      success: function(json){
-        mangaListObject = json;
-        console.log(mangaListObject);
-        if($.isEmptyObject(mangaListObject)){
-
-          $("#mangaThumbnails").append("<div id='nothing'>GET CUCKED</div>");
-
-        }else{
-          $.each(mangaListObject, function(index, element){
-
-            //console.log(index);
-
-            if(index > ((-1) + (page * 10)) && index < (10 + (page * 10))){
-              $("#mangaThumbnails").append("<div id='imgdiv'> <img id='" + element.mangaId + "' src='" + element.cover + "'/></div>");
-            }
-          });
-        }
-      }
-    });
   });
 
 
